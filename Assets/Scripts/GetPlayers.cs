d8c2441951248e03af69ef1711c237f258197d59
HMasataka <masataka.hisasue@gmail.com>
2025-03-16 22:47:52 +0900

    プレイヤーの情報取得スクリプトサンプル

---
+++ Assets/Scripts/GetPlayers.cs
@@ -0,0 +1,30 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class GetPlayers : UdonSharpBehaviour
+{
+    VRCPlayerApi[] players = new VRCPlayerApi[30];
+
+    void Start()
+    {
+        VRCPlayerApi.GetPlayers(players);
+
+        foreach (VRCPlayerApi player in players)
+        {
+            if (player == null) continue;
+            Debug.Log(player.displayName);
+        }
+    }
+    void Update()
+    {
+        VRCPlayerApi.GetPlayers(players);
+
+        foreach (VRCPlayerApi player in players)
+        {
+            if (player == null) continue;
+            Debug.Log(player.displayName);
+            Debug.Log(player.GetPosition());
+        }
+    }
+}
