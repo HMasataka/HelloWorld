5d9bb365d6ea0812281e46bb98e8694fdc9f722a
HMasataka <masataka.hisasue@gmail.com>
2025-03-16 22:03:25 +0900

    Feat teleport

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Teleport : UdonSharpBehaviour
+{
+    [SerializeField] private GameObject teleportPointTo;
+    public override void Interact()
+    {
+        Networking.LocalPlayer.TeleportTo(teleportPointTo.transform.position, teleportPointTo.transform.rotation);
+    }
+}
