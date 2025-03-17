20b12dffab0f6b7d8eaffae174476242f605441a
HMasataka <masataka.hisasue@gmail.com>
2025-03-17 22:58:22 +0900

    Joinしたときのログ表示

---
+++ Assets/Scripts/SetText.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+
+public class SetText : UdonSharpBehaviour
+{
+    private TMP_Text text;
+    void Start()
+    {
+        text = GameObject.Find("Content").GetComponent<TMP_Text>();
+        text.text = "";
+    }
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        text.text = "Hello, " + player.displayName + "! \r\n";
+    }
+}
