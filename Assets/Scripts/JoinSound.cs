2561dbd023a610b480da5ae3ed0e084b39a882ad
HMasataka <masataka.hisasue@gmail.com>
2025-04-25 20:33:52 +0900

    Join Sound

---
+++ Assets/Scripts/JoinSound.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class JoinSound : UdonSharpBehaviour
+{
+    AudioSource audioSource;
+    AudioClip audioClip;
+    //--------------------------------------- 
+    void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+        audioClip = audioSource.clip;
+    }
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        PlaySound();
+    }
+
+    public void PlaySound()
+    {
+        audioSource.PlayOneShot(audioClip);
+    }
+}
