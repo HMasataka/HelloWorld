08ce8f87dc7c373c189932f66a9ac852e2eeb97a
HMasataka <masataka.hisasue@gmail.com>
2025-04-25 20:55:26 +0900

    HUD

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,16 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class HUD : UdonSharpBehaviour
+{
+    //--------------------------------------- 
+    private void Update()
+    {
+        if (!Utilities.IsValid(Networking.LocalPlayer)) { return; }
+
+        this.transform.position = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+        this.transform.rotation = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation;
+    }
+}
