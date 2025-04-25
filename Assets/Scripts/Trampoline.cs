6d8fb9e059a26e6ae739d4eb7ab14a877ac135fd
HMasataka <masataka.hisasue@gmail.com>
2025-04-25 21:27:48 +0900

    トランポリン

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Trampoline : UdonSharpBehaviour
+{
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (!Utilities.IsValid(Networking.LocalPlayer)) { return; }
+        if (!player.isLocal) { return; }
+
+        Vector3 InVelocity = Networking.LocalPlayer.GetVelocity();
+        Vector3 localInVelocity = Quaternion.Inverse(this.transform.rotation) * InVelocity;
+        Vector3 localOutVelocity = new Vector3(localInVelocity.x, -localInVelocity.y, localInVelocity.z);
+        Vector3 OutVelocity = this.transform.rotation * localOutVelocity;
+
+        Networking.LocalPlayer.SetVelocity(OutVelocity);
+    }
+}
