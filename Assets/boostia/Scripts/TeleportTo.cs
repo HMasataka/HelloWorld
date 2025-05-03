be5b9ea495c3ffbeabd51806bc070c4916df8a38
HMasataka <masataka.hisasue@gmail.com>
2025-05-04 00:30:31 +0900

    import boostia package

---
+++ Assets/boostia/Scripts/TeleportTo.cs
@@ -0,0 +1,15 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace boostia.common
+{
+    public class TeleportTo : UdonSharpBehaviour
+    {
+        [SerializeField] private GameObject teleportTo;
+        public override void Interact()
+        {
+            Networking.LocalPlayer.TeleportTo(teleportTo.transform.position, teleportTo.transform.rotation);
+        }
+    }
+}
