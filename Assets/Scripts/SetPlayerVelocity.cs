4761b094b4a9aaebcb5af74896d65d33090241a6
HMasataka <masataka.hisasue@gmail.com>
2025-05-04 00:31:26 +0900

    Feature set player velocity

---
+++ Assets/Scripts/SetPlayerVelocity.cs
@@ -0,0 +1,53 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[RequireComponent(typeof(VRC_Pickup))]
+public class SetPlayerVelocity : UdonSharpBehaviour
+{
+    [SerializeField] float Velocity;
+    VRC_Pickup pickup;
+    bool isDoubleJump = false;
+
+    void Start()
+    {
+        pickup = GetComponent<VRC_Pickup>();
+    }
+
+    void Update()
+    {
+        if (!IsJumped())
+        {
+            isDoubleJump = false;
+        }
+    }
+
+    public override void Interact()
+    {
+        pickup = GetComponent<VRC_Pickup>();
+    }
+
+    // AutoHoldがYesのPickupオブジェクトをつかんでいる時、トリガーを引いた際に呼ばれる関数
+    public override void OnPickupUseDown()
+    {
+        if (isDoubleJump) return;
+        if (IsJumped()) isDoubleJump = true;
+        Impulse();
+    }
+
+    void Impulse()
+    {
+        Vector3 impulse = transform.forward * Velocity;
+        Networking.LocalPlayer.SetVelocity(impulse);
+    }
+
+    bool IsJumped()
+    {
+        var player = pickup.currentPlayer;
+        if (!Utilities.IsValid(player)) return false;
+
+        return !player.IsPlayerGrounded();
+    }
+}
