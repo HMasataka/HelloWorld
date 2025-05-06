2e3fe625d21f8353ee194d1d899eb59ef1163a14
HMasataka <masataka.hisasue@gmail.com>
2025-05-06 17:05:36 +0900

    消えるHUDを作成

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,16 +1,35 @@
 ﻿using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
 
 public class HUD : UdonSharpBehaviour
 {
-    //--------------------------------------- 
+    [SerializeField]
+    Image ProgressBar;
+
+    [SerializeField]
+    GameObject message;
+
+    private float WaitTime = 10.0f;
+    private float ProgressTime = 0f;
+
     private void Update()
     {
         if (!Utilities.IsValid(Networking.LocalPlayer)) { return; }
 
         this.transform.position = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
         this.transform.rotation = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation;
+
+        if (ProgressTime >= WaitTime)
+        {
+            message.SetActive(false);
+            ProgressBar.fillAmount = 0.0f;
+            return;
+        }
+
+        ProgressTime += Time.deltaTime;
+        ProgressBar.fillAmount = 1.0f - (ProgressTime / WaitTime);
     }
 }
---
+++ Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PlayerCollider : UdonSharpBehaviour
+{
+    [SerializeField]
+    public int TeamLayer;
+
+    [SerializeField]
+    public int EnemyLayer;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        // プレイヤー自体のColliderが衝突した場合、 引数のotherはnullとなっているのでnullチェックして回避
+        if (other != null)
+        {
+            // otherのレイヤーが敵チームレイヤー番号と一致したらHit判定
+            if (other.gameObject.layer == EnemyLayer)
+            {
+                Debug.Log("Hit!!");
+            }
+
+        }
+    }
+}
